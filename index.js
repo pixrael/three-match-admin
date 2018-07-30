a4a5b4f8d118d366013f9d885bca95b63af77772
Set Israel Ortiz Monasterio <pixrael777@gmail.com>
2018-07-29 23:59:45 -0300

    Added port and server ip

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
 var Setup = require('./initialSetup');
 var initialSetup = Setup.initSetup;
 
@@ -9,4 +12,4 @@ app.get('/', function (req, res) {
    res.send(initialSetup);
 });
 
-app.listen(3000);
+app.listen(server_port, server_ip_address);
