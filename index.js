885a717c191c9d8208fa4e602b39c08c84a39cbf
Set Israel Ortiz Monasterio <pixrael777@gmail.com>
2018-07-30 22:42:15 -0300

    test line

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+console.log('HELLO THERE');
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
